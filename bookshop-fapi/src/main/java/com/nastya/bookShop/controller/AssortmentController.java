f02e5a21dd49017ea0aafce497f82e393231ef29
nastyalevk <levkovich.astya@gmail.com>
2021-02-15 12:54:53 +0300

    Update AssortmentController.java

--- bookshop-fapi/src/main/java/com/nastya/bookShop/controller/AssortmentController.java
+++ bookshop-fapi/src/main/java/com/nastya/bookShop/controller/AssortmentController.java
@@ -2,8 +2,6 @@ package com.nastya.bookShop.controller;
 
 import com.nastya.bookShop.model.Assortment.AssortmentDto;
 import com.nastya.bookShop.service.api.AssortmentService;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
